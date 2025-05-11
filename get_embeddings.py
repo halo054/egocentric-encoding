ff2f604d1e846b09fe068925f3fa1d98ff182f46
halo054 <139146470+halo054@users.noreply.github.com>
2025-05-10 20:00:08 -0400

    Update get_embeddings.py

--- get_embeddings.py
+++ get_embeddings.py
@@ -1,7 +1,7 @@
 from dataset import EgoExoDataset
 from tqdm import tqdm
 from pathlib import Path
-
+import time
 import data
 import torch
 from models import imagebind_model
@@ -24,18 +24,26 @@ make_directory('embeddings')
 make_directory('embeddings/ego-original')
 make_directory('embeddings/exo')
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Instantiate model
 model = imagebind_model.imagebind_huge(pretrained=True)
+
+ngpus = torch.cuda.device_count()
+if ngpus > 1:
+    print("using",ngpus,"GPUs" )
+    model = torch.nn.DataParallel(model)
+
 model.eval()
 model.to(device)
 
 seen_ego_takes = [f.name.replace('.pt', '') for f in Path('embeddings/ego-original').iterdir() if f.is_file()]
 seen_exo_takes = [f.name.replace('.pt', '') for f in Path('embeddings/exo').iterdir() if f.is_file()]
 seen_takes = [f for f in seen_ego_takes if f in seen_exo_takes]
-dataset = EgoExoDataset(TAKES_FILENAME, SPLITS_FILENAME, DATA_ROOT_DIR, split='test', skip_takes=seen_takes)
+dataset = EgoExoDataset(TAKES_FILENAME, SPLITS_FILENAME, DATA_ROOT_DIR, split='test',skip_takes=seen_takes)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
+
+
 
 with torch.no_grad():
     for batch in tqdm(dataloader):
@@ -44,8 +52,12 @@ with torch.no_grad():
         ego_data = ego_data[0]
         exo_data = exo_data[0]
 
+        #print("time0",time.time())
+        print(take_uid)
+
         ego_embeddings = model({ModalityType.VISION: ego_data})[ModalityType.VISION]
         torch.save(ego_embeddings,f'embeddings/ego-original/{take_uid}.pt')
         exo_embeddings = model({ModalityType.VISION: exo_data})[ModalityType.VISION]
         torch.save(exo_embeddings,f'embeddings/exo/{take_uid}.pt')
 
+        #print("time1",time.time())
