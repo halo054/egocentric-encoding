950ba4f59cf8ed7f2a16c1b4d3614a44875e6c6a
halo054 <139146470+halo054@users.noreply.github.com>
2025-05-18 23:51:31 -0400

    Update dataset.py

--- Finalized/Codes/dataset.py
+++ Finalized/Codes/dataset.py
@@ -54,8 +54,8 @@ class EgoExoDataset(Dataset):
         exo_video_filename = self.data_root_dir + "/" + self.entries[idx]['root_dir'] + '/' \
         +  self.entries[idx]['frame_aligned_videos'][exo_keyname]['0']['relative_path']
         # return (ego_video_filename, exo_video_filename, self.entries[idx]['take_uid'])
-        ego_video_data = 0 #utils.load_and_transform_video_data([ego_video_filename], self.device, 2, 5)[0]
-        exo_video_data =  0 #utils.load_and_transform_video_data([exo_video_filename], self.device, 2, 5)[0]
+        ego_video_data = utils.load_and_transform_video_data([ego_video_filename], self.device, 2, 5)[0]
+        exo_video_data =  utils.load_and_transform_video_data([exo_video_filename], self.device, 2, 5)[0]
         #print(idx)
         return (ego_video_data, exo_video_data, self.entries[idx]['take_uid'])
 
